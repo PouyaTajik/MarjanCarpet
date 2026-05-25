f31a899275f5439f441ccdc13b65adeb7228923b
Pouya Tajik <pouya.tajik11>
2026-05-25 15:57:48 +0200

    Rebuild with correct base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MarjanCarpet/',  // مسیر پایه باید نام مخزن باشد
+  base: '/MarjanCarpet/',
   plugins: [react()],
 })
